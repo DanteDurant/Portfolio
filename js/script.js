0277569aa488cd051f9d856761c4bbe5297f419b
DanteDurant <rathersafe@protonmail.com>
2022-01-20 11:34:47 +0200

    LinkedIn link update

--- js/script.js
+++ js/script.js
@@ -14,22 +14,6 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
-    // e.preventDefault();
-    // const href = link.getAttribute("href");
-
-    // // Scroll back to top
-    // if (href === "#")
-    //   window.scrollTo({
-    //     top: 0,
-    //     behavior: "smooth",
-    //   });
-
-    // // Scroll to other links
-    // if (href !== "#" && href.startsWith("#")) {
-    //   const sectionEl = document.querySelector(href);
-    //   sectionEl.scrollIntoView({ behavior: "smooth" });
-    // }
-
     // Close mobile naviagtion
     if (link.classList.contains("main-nav-link"))
       headerEl.classList.toggle("nav-open");
@@ -57,7 +41,7 @@ const obs = new IntersectionObserver(
     // In the viewport
     root: null,
     threshold: 0,
-    rootMargin: "-80px",
+    rootMargin: "-50px",
   }
 );
 obs.observe(sectionIntroEl);
